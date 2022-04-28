ad5989a2cd1b8b37110d502f0497cb60a37384cd
Szymon <regicsmaster@gmail.com>
2022-04-28 19:11:02 +0200

    Added Vector methods to lab6

--- LAB6/sr_w_odchylenie.py
+++ LAB6/sr_w_odchylenie.py
@@ -1,3 +1,5 @@
+import numpy as np
+import math as m
 def srednia(list1):
 	sum = 0
 	for x in list1:
@@ -13,3 +15,26 @@ def warjancja(list1):
 
 def odchylenie(list1):
 	return pow(warjancja(list1), 1/2)
+
+def srednia_matrix(lista):
+    ones = np.ones((len(lista),1))
+    return float(1/len(lista))*np.dot(np.array(lista),ones)[0]
+
+def wariancja_matrix(lista):
+    srednia = srednia_matrix(lista)
+    ones = np.ones((1,len(lista)))*srednia
+    minus = np.array(lista) - ones
+    return float(1/len(lista))*np.dot(minus[0],minus[0].T)
+
+def odchylenie_matrix(lista):
+    return m.sqrt(wariancja_matrix(lista))
+
+
+lista = [5,5,5,5]
+print(srednia(lista))
+print(warjancja(lista))
+print(odchylenie(lista))
+
+print(srednia_matrix(lista))
+print(wariancja_matrix(lista))
+print(odchylenie_matrix(lista))
